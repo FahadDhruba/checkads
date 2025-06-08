e6e249efc2ee477f6d2534f15d634d29321c26f0
fahaddhruba <dhruba.fahad2004@gmail.com>
2025-06-08 21:04:19 +0600

    scrip

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8627748653559278" crossorigin="anonymous"></script>
+      </head>
       <body className="antialiased">
         {children}
       </body>
